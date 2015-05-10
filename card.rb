5b28dbbad0722ca1c6e72b2cc0a5223a4a89084f
Amie Wilt <amie.wilt@gmail.com>
2015-05-10 10:10:13 -0400

    added def value for worth of cards

--- card.rb
+++ card.rb
@@ -1,8 +1,20 @@
 class Card
 
+  attr_readers :suit
+
   def initialize(suit, value)
     @suit = suit
     @value = value
+  end
+
+  def value
+    return 10 if ["J", "Q", "K"].include?(@value)
+    return 11 if @value == "A"
+    return @value
+  end
+
+  def to_s
+    "#{@value}-#{suit}"
   end
 end
 
