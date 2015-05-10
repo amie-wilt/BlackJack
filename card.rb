4b6984166a1d36e37285f03724ed4437c0e6d983
Amie Wilt <amie.wilt@gmail.com>
2015-05-10 09:56:37 -0400

    did card and deck classes, working on game

--- card.rb
+++ card.rb
@@ -1,26 +1,8 @@
 class Card
 
-    def initialize(suit, value)
-        @card = card
-    end
-
-    # def name
-    #     puts value + " of " suit
-    # end
-
-
-
-
-    suits = [:hearts, :diamonds, :spades, :clubs]
-    suits.each do |suit|
-        (2..10).each do |value|
-            @cards << Card.new(suit, value)
-        end
-      @cards << Card.new(suit, "J")
-      @cards << Card.new(suit, "Q")
-      @cards << Card.new(suit, "K")
-      @cards << Card.new(suit, "A")
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
 end
 
-
-end
--- deck.rb
+++ deck.rb
@@ -1,23 +1,33 @@
-class Deck
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'card'
 
-    SUIT = [:hearts, :diamonds, :spades, :clubs]
-    VALUE = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+class Deck < Card
 
-    def initialize(suit, value)
-        @suit = suit
-        @value = value
+  def initialize
+    @cards = []
+    suits = [:hearts, :diamonds, :spades, :clubs]
+    suits.each do |suit|
+      (2..10).each do |value|
+        @cards << Card.new(suit, value)
+      end
+      @cards << Card.new(suit, "J")
+      @cards << Card.new(suit, "Q")
+      @cards << Card.new(suit, "K")
+      @cards << Card.new(suit, "A")
     end
+  end
 
+  def shuffle
+    @cards.shuffle!
+  end
 
+  def draw
+    drawn_card = @cards.shift
+    puts drawn_card
+  end
 
-#     suits = [:hearts, :diamonds, :spades, :clubs]
-# suits.each do |suit|
-#   (2..10).each do |value|
-#     @cards << Card.new(suit, value)
-#   end
-#   @cards << Card.new(suit, "J")
-#   @cards << Card.new(suit, "Q")
-#   @cards << Card.new(suit, "K")
-#   @cards << Card.new(suit, "A")
 end
-end
+
+deck = Deck.new
+puts deck.shuffle
+puts deck.draw.to_s
